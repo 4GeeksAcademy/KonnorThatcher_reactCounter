977c736105c4c90241141915240953de971c0f9a
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-07-04 01:42:18 +0000

    Counter is functional. Also cleaned up JS code

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0)
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log("Why does it break?");
+            setCount(count + 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    })
+
     const sixDigits = (n) => {
         let arr = n.toString().split('');
         while (arr.length < 6) arr.unshift('0');
-        return arr;
+        return arr.map((n)=> {
+            return (
+                <div className='ms-2 px-2 border-start border-end border-3 border-secondary rounded bg-dark'>
+                    <h1 className='text-light px-1'>{n}</h1>
+                </div>
+            );
+        });
     }
     
-    const htmlNum = sixDigits(count).map((n) => {
-        return (
-            <div className='ms-2 px-2 border-start border-end border-3 border-secondary rounded bg-dark'>
-                <h1 className='text-light'>{n}</h1>
-            </div>
-        );
-    })
-
-    const plusOne = () => {
-        setCount(count + 1);
-    }
-
     return (
         <div className='mt-5'>
-            <div className='d-flex flex-row justify-content-center mb-3'>{htmlNum}</div>
-            <button onClick={plusOne}>Click me you goofy idiot!</button>
+            <div className='d-flex flex-row justify-content-center mb-3'>{sixDigits(count)}</div>
         </div>
     )
 }
